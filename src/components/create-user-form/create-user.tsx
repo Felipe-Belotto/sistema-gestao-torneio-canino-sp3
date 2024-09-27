e849b581974a9f170e8bdaef4ede3df30df8facc
Felipe-Belotto <felipeeduardo817@gmail.com>
2024-09-26 22:13:25 -0300

    Refactor code to remove unused imports and fix formatting

--- src/components/create-user-form/create-user.tsx
+++ src/components/create-user-form/create-user.tsx
@@ -1,31 +1,17 @@
-<<<<<<< HEAD:src/components/create-user.tsx
 "use client";
-import { createUser } from "@/lib/prisma";
+import { createUser, getAllUsers, getUserById } from "@/lib/prisma";
 import type { UserProps } from "@/lib/types";
 import type React from "react";
 import { useState } from "react";
 import { RadioSex } from "./radio-sex";
-=======
-'use client'
-import { createUser, getAllUsers, getUserById } from '@/lib/prisma';
-import { UserProps } from '@/lib/types';
-import React, { useState } from 'react'
-import { RadioSex } from './radio-sex';
-
->>>>>>> 0a351a669ea8f74ebb2c1cd214b742eaf5a74814:src/components/create-user-form/create-user.tsx
 
 function CreateUserForm() {
   const [formData, setFormData] = useState<UserProps>({
     name_conductor: "",
     name_dog: "",
     age_dog: 0,
-<<<<<<< HEAD:src/components/create-user.tsx
-    instituition: "",
+    institution: "",
     sex_dog: "",
-=======
-    institution: '',
-    sex_dog: ''
->>>>>>> 0a351a669ea8f74ebb2c1cd214b742eaf5a74814:src/components/create-user-form/create-user.tsx
   });
 
   const handleInputFormChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -40,47 +26,26 @@ function CreateUserForm() {
     e.preventDefault();
 
     try {
-<<<<<<< HEAD:src/components/create-user.tsx
-      createUser(formData);
-      console.log(formData);
-=======
-      await createUser(formData)
->>>>>>> 0a351a669ea8f74ebb2c1cd214b742eaf5a74814:src/components/create-user-form/create-user.tsx
+      await createUser(formData);
       setFormData({
         name_conductor: "",
         name_dog: "",
         age_dog: 0,
-<<<<<<< HEAD:src/components/create-user.tsx
-        instituition: "",
+        institution: "",
         sex_dog: "",
       });
-=======
-        institution: '',
-        sex_dog: ''
-      })
-    
->>>>>>> 0a351a669ea8f74ebb2c1cd214b742eaf5a74814:src/components/create-user-form/create-user.tsx
     } catch (error) {
       throw new Error("Failed to create user");
     }
   };
 
   return (
-<<<<<<< HEAD:src/components/create-user.tsx
     <div className="w-full flex flex-col  items-center">
       <h1 className="mb-4 text-xl">Formulario de inscrição</h1>
 
       <form onSubmit={handleSubmit} className="flex flex-col gap-2 ">
         <div className="flex flex-col gap-2">
-          <label htmlFor="instituition">Instituição/Cidade</label>
-=======
-    <div className='w-full flex flex-col  items-center'>
-      <h1 className='mb-4 text-xl'>Formulario de inscrição</h1>
-      
-      <form onSubmit={handleSubmit} className='flex flex-col gap-2 '>
-      <div className='flex flex-col gap-2'>
           <label htmlFor="institution">Instituição/Cidade</label>
->>>>>>> 0a351a669ea8f74ebb2c1cd214b742eaf5a74814:src/components/create-user-form/create-user.tsx
           <input
             type="text"
             id="institution"
@@ -133,16 +98,16 @@ function CreateUserForm() {
           handleChange={handleInputFormChange}
         />
 
-<<<<<<< HEAD:src/components/create-user.tsx
         <button type="submit" className="p-4 py-2 rounded-lg bg-green-400 w-60">
           Criar Usuário
         </button>
-=======
-        <button type="submit" className='p-4 py-2 rounded-lg bg-green-400 w-60'>Criar Usuário</button>
-        
->>>>>>> 0a351a669ea8f74ebb2c1cd214b742eaf5a74814:src/components/create-user-form/create-user.tsx
       </form>
-      <button onClick={() => getAllUsers() } className='p-4 py-2 rounded-lg bg-blue-400 w-60 mt-8'>Buscar usuarios</button>
+      <button
+        onClick={() => getAllUsers()}
+        className="p-4 py-2 rounded-lg bg-blue-400 w-60 mt-8"
+      >
+        Buscar usuarios
+      </button>
     </div>
   );
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,16 +1,13 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import clsx, { type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { getUserById } from "./prisma";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
-<<<<<<< HEAD
-=======
 
 export async function getById() {
-  const user = await getUserById("eacce6c8-1bb9-455e-b566-afe573486003")
+  const user = await getUserById("eacce6c8-1bb9-455e-b566-afe573486003");
   console.log(user);
-  return user
+  return user;
 }
-
->>>>>>> 0a351a669ea8f74ebb2c1cd214b742eaf5a74814
